00bb57a7b66e5a0cb43ec75f18c1f43a3d265d3f
Elena Popova <elipopovadev@gmail.com>
2021-03-10 00:25:31 +0200

    RhombusOfStars

--- WorkingWithAbstraction-Lab/RhombusOfStars/Program.cs
+++ WorkingWithAbstraction-Lab/RhombusOfStars/Program.cs
@@ -2,11 +2,51 @@
 
 namespace RhombusOfStars
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            Rhombus currentRhombus = new Rhombus(n);
+            currentRhombus.PrintTop();
+            currentRhombus.PrintMiddle();
+            currentRhombus.PrintBottom();
+        }
+
+
+        public class Rhombus
+        {
+            public Rhombus(int size)
+            {
+                this.Size = size;
+            }
+
+            public int Size { get; }
+
+            public void PrintTop()
+            {                
+                for (int row = 0; row < this.Size - 1; row++)
+                {
+                    Console.Write(new string(' ', this.Size - 1 - row));
+                    Console.WriteLine(string.Join(' ', new string('*', row + 1).ToCharArray()));                                  
+                }             
+            }
+            public void PrintMiddle()
+            {
+                Console.WriteLine(string.Join(' ', new string('*', this.Size).ToCharArray()));
+            }
+
+            public void PrintBottom()
+            {
+                for (int row = this.Size-2; row >= 0; row--)
+                {
+                    Console.Write(new string(' ', this.Size - 1 - row));
+                    Console.WriteLine(string.Join(' ', new string('*', row + 1).ToCharArray()));
+                }
+            }
         }
     }
 }
+
+
+
