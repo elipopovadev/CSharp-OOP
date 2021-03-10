cdc6f245e36902aac439cd79d189d7b34ba2e89c
Elena Popova <elipopovadev@gmail.com>
2021-03-11 00:12:03 +0200

    HotelReservation

---
+++ WorkingWithAbstraction-Lab/HotelReservation/PriceCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation
+{
+    public static class PriceCalcualtor
+    {
+        public static decimal GetTotalPrice(decimal pricePerDay, int numberOfDays, Season season, DiscountType discountType)
+        {        
+            decimal priceBeforeDiscount = pricePerDay * numberOfDays * (int)season;
+            decimal discount = priceBeforeDiscount * (int)discountType / 100;
+            decimal totalPrice = priceBeforeDiscount - discount;
+            return totalPrice;
+        }
+    }
+}
--- WorkingWithAbstraction-Lab/HotelReservation/Program.cs
+++ WorkingWithAbstraction-Lab/HotelReservation/Program.cs
@@ -2,11 +2,30 @@
 
 namespace HotelReservation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-         
+            string[] input = Console.ReadLine().Split();
+            decimal price = decimal.Parse(input[0]);
+            int numberOfDays = int.Parse(input[1]);
+            Season season = Enum.Parse<Season>(input[2]);
+
+            decimal totalPrice = 0;
+            if (input.Length == 4)
+            {
+                DiscountType discount = Enum.Parse<DiscountType>(input[3]);
+                totalPrice = PriceCalcualtor.GetTotalPrice(price, numberOfDays, season, discount);
+            }
+
+            else
+            {
+                DiscountType discount = 0;
+                totalPrice = PriceCalcualtor.GetTotalPrice(price, numberOfDays, season, discount);
+            }
+
+            Console.WriteLine($"{totalPrice:f2}");
         }
     }
 }
+
