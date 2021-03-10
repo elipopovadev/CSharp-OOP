d2dfc3dc000f7c05b0a3c9350d17551667c3765e
Elena Popova <elipopovadev@gmail.com>
2021-03-10 16:53:40 +0200

    Add class Rhombus

---
+++ WorkingWithAbstraction-Lab/RhombusOfStars/Rhombus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    public class Rhombus
+    {
+        public Rhombus(int size)
+        {
+            this.Size = size;
+        }
+
+        public int Size { get; }
+
+        public void PrintTop()
+        {
+            for (int row = 0; row < this.Size - 1; row++)
+            {
+                Console.Write(new string(' ', this.Size - 1 - row));
+                Console.WriteLine(string.Join(' ', new string('*', row + 1).ToCharArray()));
+            }
+        }
+
+        public void PrintMiddle()
+        {
+            Console.WriteLine(string.Join(' ', new string('*', this.Size).ToCharArray()));
+        }
+
+        public void PrintBottom()
+        {
+            for (int row = this.Size - 2; row >= 0; row--)
+            {
+                Console.Write(new string(' ', this.Size - 1 - row));
+                Console.WriteLine(string.Join(' ', new string('*', row + 1).ToCharArray()));
+            }
+        }
+    }
+}
---
+++ WorkingWithAbstraction-Lab/RhombusOfStars/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Rhombus currentRhombus = new Rhombus(n);
+            currentRhombus.PrintTop();
+            currentRhombus.PrintMiddle();
+            currentRhombus.PrintBottom();
+        }
+    }
+}
+
+
+
+
