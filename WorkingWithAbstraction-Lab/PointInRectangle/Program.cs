76105b8b44e5ea20feb689928e2b646aa1a1e24e
Elena Popova <elipopovadev@gmail.com>
2021-03-17 00:00:55 +0200

    PointInRectangle

--- WorkingWithAbstraction-Lab/PointInRectangle/Program.cs
+++ WorkingWithAbstraction-Lab/PointInRectangle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PointInRectangle
 {
@@ -6,7 +7,24 @@ namespace PointInRectangle
     {
         static void Main(string[] args)
         {
-           
+            int[] rectangleInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int topLeftX = rectangleInfo[0];
+            int topLeftY = rectangleInfo[1];
+            Point topLeftPoint = new Point(topLeftX, topLeftY);
+            int bottomRightX = rectangleInfo[2];
+            int bottomRightY = rectangleInfo[3];
+            Point bottomRightPoint = new Point(bottomRightX, bottomRightY);
+            Rectangle rectangle = new Rectangle(topLeftPoint, bottomRightPoint);
+
+            int numberOfPoints = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                int[] pointInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int x = pointInfo[0];
+                int y = pointInfo[1];
+                Point newPoint = new Point(x, y);
+                Console.WriteLine(rectangle.Contains(newPoint));
+            }          
         }
     }
 }
---
+++ WorkingWithAbstraction-Lab/PointInRectangle/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿namespace PointInRectangle
+{
+    class Rectangle
+    {
+        public Rectangle(Point topLeftPoint, Point bottomRightPoint)
+        {
+            this.TopLeftPoint = topLeftPoint;
+            this.BottomRightPoint = bottomRightPoint;
+        }
+
+        public Point TopLeftPoint  { get; set; }
+        public Point BottomRightPoint { get; set; }
+
+        public bool Contains (Point point)
+        {
+            bool XIsInside = false;
+            bool YIsInside = false;
+            if ( point.X >= TopLeftPoint.X && point.X <= BottomRightPoint.X)
+            {
+                XIsInside = true;
+            }
+
+            if(point.Y <= TopLeftPoint.Y && point.Y >= BottomRightPoint.Y)
+            {
+                YIsInside = true;
+            }
+
+            return XIsInside && YIsInside;
+        }
+    }
+}
