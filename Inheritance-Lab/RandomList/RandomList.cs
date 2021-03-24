a0909f7df0960d9279ad4523e39f38c5ec666a7d
Elena Popova <elipopovadev@gmail.com>
2021-03-24 22:53:00 +0200

    RandomList

---
+++ Inheritance-Lab/RandomList/RandomList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {       
+        Random randomGenerator = new Random();
+        public string RandomString()
+        {
+           int randomIndex = randomGenerator.Next(this.Count);
+            return this[randomIndex];
+        }          
+    }
+}
---
+++ Inheritance-Lab/RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("Gosho");
+            randomList.Add("Misho");
+            randomList.Add("Pesho");
+           
+            string randomString = randomList.RandomString();
+            Console.WriteLine(randomString);                  
+        }
+    }
+}
