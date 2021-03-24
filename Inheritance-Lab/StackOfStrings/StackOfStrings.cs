4488aefe41a98d7e94192962dcdce5cf63271dd3
Elena Popova <elipopovadev@gmail.com>
2021-03-25 00:03:26 +0200

    Stack Of String

---
+++ Inheritance-Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    class StackOfStrings<T> : Stack<T>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddRange(IEnumerable<T> strings)
+        {
+            foreach (var item in strings)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ Inheritance-Lab/StackOfStrings/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings<string> stringStack = new StackOfStrings<string>();
+            stringStack.Push("Gosho");
+            stringStack.Push("Pesho");
+            Console.WriteLine(stringStack.IsEmpty());
+            Stack<string> stringStackForAdd = new Stack<string>();
+            stringStackForAdd.Push("Elena");
+            stringStackForAdd.Push("Ana");
+            stringStack.AddRange(stringStackForAdd);
+            Console.WriteLine(string.Join(" ",stringStack));
+            Console.WriteLine(stringStack.IsEmpty());
+
+            StackOfStrings<int> intStack = new StackOfStrings<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            Stack<int> intStackForAdd = new Stack<int>();
+            intStackForAdd.Push(3);
+            intStackForAdd.Push(4);
+            intStack.AddRange(intStackForAdd);
+            Console.WriteLine(string.Join(" ", intStack));
+            Console.WriteLine(intStack.IsEmpty());
+        }
+    }
+}
