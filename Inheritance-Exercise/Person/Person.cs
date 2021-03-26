04aa3d29fa79e0e31edfb2fd2da2d88adf4efacd
Elena Popova <elipopovadev@gmail.com>
2021-03-26 11:59:48 +0200

    Person - how to override property

---
+++ Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; }
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
