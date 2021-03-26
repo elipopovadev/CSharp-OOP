18033b443cc9ea6cf7952f7b7bb6da9590ec2075
Elena Popova <elipopovadev@gmail.com>
2021-03-27 00:10:46 +0200

    Need for Speed

---
+++ Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(300, 6);
+            Console.WriteLine(car.Fuel); // 6
+            car.Drive(100);
+            Console.WriteLine(car.Fuel); // 3
+
+            Car sportCar = new SportCar(500, 10);
+            Console.WriteLine(sportCar.Fuel); // 10
+            sportCar.Drive(100);
+            Console.WriteLine(sportCar.Fuel); // 0    
+            sportCar.Drive(10);
+        }
+    }
+}
---
+++ Inheritance-Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double defaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        public virtual double FuilConsumption { get => defaultFuelConsumption; }
+
+        public void Drive(double kilometers)
+        {
+            if (this.FuilConsumption * kilometers / 100 <= this.Fuel)
+            {
+                this.Fuel -= this.FuilConsumption * kilometers / 100;
+            }
+
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
