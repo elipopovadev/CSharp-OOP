d384dc67d79d4a008de9e15d733436f308d5d00a
Elena Popova <elipopovadev@gmail.com>
2021-03-27 14:16:16 +0200

    Restaurant

---
+++ Inheritance-Exercise/Restaurant/Cake.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+   public class Cake : Dessert
+    {
+        private const decimal cakePrice = 5;
+        private const double cakeGrams = 250;
+        private const double cakeCalories = 1000;
+      
+        public Cake(string name) : base(name, cakePrice, cakeGrams, cakeCalories)
+        {           
+        }
+    }
+}
---
+++ Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double  coffeMilliliteres = 50;
+        private const decimal cofeePrice = 3.50m;
+
+        public Coffee(string name, double caffeine) : base(name, cofeePrice, coffeMilliliteres)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
