f129237b9b7dfa2bb770f24a2805d28a40c8cad6
Elena Popova <elipopovadev@gmail.com>
2021-03-30 11:48:34 +0200

    Salary

---
+++ Encapsulation-Lab/Salary/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace Salary
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal parcentage)
+        {
+            if(this.Age < 30)
+            {
+                this.Salary += parcentage/100 / 2 * this.Salary;
+            }
+            else
+            {
+                this.Salary += parcentage / 100 * this.Salary;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
---
+++ Encapsulation-Lab/Salary/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using Salary;
+using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+   public class StartUp
+    {
+            static void Main(string[] args)
+            {
+                int lines = int.Parse(Console.ReadLine());
+                var listWithPeople = new List<Person>();
+                for (int i = 0; i < lines; i++)
+                {
+                    string[] input = Console.ReadLine().Split();
+                    string firstName = input[0];
+                    string lastName = input[1];
+                    int age = int.Parse(input[2]);
+                    decimal salary = decimal.Parse(input[3]);
+                    var newPerson = new Person(firstName, lastName, age, salary);
+                    listWithPeople.Add(newPerson);
+                }
+
+                decimal parcentage = decimal.Parse(Console.ReadLine());
+                foreach (var person in listWithPeople)
+                {
+                   person.IncreaseSalary(parcentage);
+                   Console.WriteLine(person);
+                }
+            }
+        }
+    }
+
