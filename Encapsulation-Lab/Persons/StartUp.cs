d15e4e2dd2175f94406658dd0ff92372c47ebcd3
Elena Popova <elipopovadev@gmail.com>
2021-03-30 10:22:56 +0200

    Persons

---
+++ Encapsulation-Lab/Persons/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using Persons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var listWithPeople = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                var newPerson = new Person(firstName, lastName, age);
+                listWithPeople.Add(newPerson);
+            }
+
+            foreach (var person in listWithPeople.OrderBy(p=> p.FirstName).ThenBy(p=>p.LastName))
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
