22eaea4481d5fc6ea79ded3a717f0147e4d4aac3
Elena Popova <elipopovadev@gmail.com>
2021-03-30 13:13:58 +0200

    Validation

--- Encapsulation-Lab/Validation/StartUp.cs
+++ Encapsulation-Lab/Validation/StartUp.cs
@@ -25,7 +25,6 @@ namespace PersonsInfo
 
                 catch (ArgumentOutOfRangeException exeption)
                 {
-
                     Console.WriteLine(exeption);
                 }               
             }
