62c8de46cff0d0833b67c1431f6bd44b08cf9544
Elena Popova <elipopovadev@gmail.com>
2021-03-30 13:04:14 +0200

    Validation

---
+++ Encapsulation-Lab/Validation/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Validation
+{
+  
+    public class Person
+    {
+        private const decimal MinSalary = 460;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            private set
+            {
+                if (value.Length < 3 || value == null)
+                {
+                    throw new ArgumentOutOfRangeException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            private set
+            {
+                if (value.Length < 3 || value == null)
+                {
+                    throw new ArgumentOutOfRangeException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => this.salary;
+            private set
+            {
+                if (value < MinSalary)
+                {
+                    throw new ArgumentOutOfRangeException("Salary cannot be less than 460 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal parcentage)
+        {
+            if (this.Age < 30)
+            {
+                this.Salary += parcentage / 100 / 2 * this.Salary;
+            }
+            else
+            {
+                this.Salary += parcentage / 100 * this.Salary;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:F2} leva.";
+        }
+    }
+}
---
+++ Encapsulation-Lab/Validation/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Validation;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var listWithPeople = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    string[] input = Console.ReadLine().Split();
+                    string firstName = input[0];
+                    string lastName = input[1];
+                    int age = int.Parse(input[2]);
+                    decimal salary = decimal.Parse(input[3]);
+                    var newPerson = new Person(firstName, lastName, age, salary);
+                    listWithPeople.Add(newPerson);
+                }
+
+                catch (ArgumentOutOfRangeException exeption)
+                {
+
+                    Console.WriteLine(exeption);
+                }               
+            }
+
+            decimal parcentage = decimal.Parse(Console.ReadLine());
+            foreach (var person in listWithPeople)
+            {
+                person.IncreaseSalary(parcentage);
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
