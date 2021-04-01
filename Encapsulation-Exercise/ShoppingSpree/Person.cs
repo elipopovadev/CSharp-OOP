4cdc03fcefe747da19c609f97110187d4bb75878
Elena Popova <elipopovadev@gmail.com>
2021-04-01 12:55:58 +0200

    Shopping Spree

---
+++ Encapsulation-Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+        public Person(string name, decimal money)
+        {
+            this.Name = name; // property
+            this.Money = money; // property
+            this.bagOfProducts = new List<Product>(); // field
+        }
+
+        public string Name
+        {
+            get 
+            {
+                return name; // field
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;  // field
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+
+        public IReadOnlyCollection<Product> BagOfProducts 
+        {
+            get
+            {
+                return bagOfProducts.AsReadOnly();
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            if(this.Money >= product.Cost)
+            {
+                bagOfProducts.Add(product);
+                this.Money -= product.Cost;
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.Name);
+            sb.Append(" - ");
+            if (this.bagOfProducts.Count == 0)
+            {
+                sb.Append("Nothing bought");
+            }
+
+            else
+            {
+                sb.Append(string.Join(", ", this.bagOfProducts.Select(pr => pr.Name)));
+            }
+           
+            return sb.ToString();
+        }
+    }
+}
---
+++ Encapsulation-Exercise/ShoppingSpree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split(';',StringSplitOptions.RemoveEmptyEntries);
+            string[] secondLine = Console.ReadLine().Split(';',StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, Person> allPeople = new Dictionary<string, Person>();
+            Dictionary<string, Product> allProducts = new Dictionary<string, Product>();
+
+            try
+            {
+                for (int i = 0; i < firstLine.Count(); i++)
+                {
+                    string[] person = firstLine[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    string name = person[0];
+                    decimal money = decimal.Parse(person[1]);
+                    Person newPerson = new Person(name, money);
+                    if (!allPeople.ContainsKey(newPerson.Name))
+                    {
+                        allPeople.Add(newPerson.Name, newPerson);
+                    }
+                }
+
+                for (int i = 0; i < secondLine.Count(); i++)
+                {
+                    string[] product = secondLine[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    string productName = product[0];
+                    decimal productCost = decimal.Parse(product[1]);
+                    Product newProduct = new Product(productName, productCost);
+                    if (!allProducts.ContainsKey(newProduct.Name))
+                    {
+                        allProducts.Add(newProduct.Name, newProduct);
+                    }
+                }
+
+                string input;
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] inputArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string personName = inputArray[0];
+                    string productName = inputArray[1];
+                    Person findedPerson = allPeople.FirstOrDefault(person => person.Key == personName).Value;
+                    Product findedProduct = allProducts.FirstOrDefault(product => product.Key == productName).Value;
+                    findedPerson.AddProduct(findedProduct);
+                }
+
+                foreach (var (stringName, person) in allPeople)
+                {
+                    Console.WriteLine(person.ToString());
+                }
+            }
+
+            catch (Exception exeption)
+            {
+                Console.WriteLine(exeption.Message);
+            }
+        }
+    }
+}
