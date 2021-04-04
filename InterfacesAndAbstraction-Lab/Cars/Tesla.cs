f422c003df4532eada58c97ac754abb0a23ba621
Elena Popova <elipopovadev@gmail.com>
2021-04-04 23:11:13 +0200

    Cars

---
+++ InterfacesAndAbstraction-Lab/Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get;}
+        public string Color { get; }
+        public int Battery { get; }
+
+        public string Start()
+        {
+            return "Engine start"; 
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine($"{ this.Start()}");
+            sb.AppendLine($"{this.Stop()}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
