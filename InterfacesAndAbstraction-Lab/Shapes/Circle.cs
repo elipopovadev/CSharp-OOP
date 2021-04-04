b95418c9b7c7bee4c939f7daa0554f00be86d26e
Elena Popova <elipopovadev@gmail.com>
2021-04-04 21:23:35 +0200

    Shapes

---
+++ InterfacesAndAbstraction-Lab/Shapes/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                if (value > 0)
+                {
+                    radius = value;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            double consoleRatio = Convert.ToDouble(4.0 / 2.0);
+            double a = consoleRatio * radius;
+            double b = radius;
+
+            for (double y = -radius; y <= radius; y++)
+            {
+                for (double x = -a; x <= a; x++)
+                {
+                    double d = (x / a) * (x / a) + (y / b) * (y / b);
+                    if (d > 1 && d < 1.2)
+                    {
+                        Console.Write('*');
+                    }
+
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ InterfacesAndAbstraction-Lab/Shapes/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public void Draw()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                Console.WriteLine(new string('*', this.Width));
+            }
+        }
+    }
+}
