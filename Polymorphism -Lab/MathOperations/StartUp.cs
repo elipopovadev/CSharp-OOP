d4daf50d510d4e5f7f063ea37bbdd8386aaa2fe7
Elena Popova <elipopovadev@gmail.com>
2021-04-13 15:35:31 +0200

    Math Operations

---
+++ Polymorphism -Lab/MathOperations/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathOperations
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations();
+            Console.WriteLine(mathOperations.Add(2, 3));
+            Console.WriteLine(mathOperations.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mathOperations.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
