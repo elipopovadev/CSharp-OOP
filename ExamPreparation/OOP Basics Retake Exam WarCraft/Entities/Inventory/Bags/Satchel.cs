38016cf19a4b40fe7654ba6063c73e0d1cefefb5
Elena Popova <elipopovadev@gmail.com>
2021-04-28 23:21:37 +0200

    Warcraft

---
+++ ExamPreparation/OOP Basics Retake Exam WarCraft/Entities/Inventory/Bags/Satchel.cs
@@ -0,0 +1,12 @@
+﻿namespace WarCroft.Entities.Inventory.Bags
+{
+   public class Satchel : Bag
+    {
+        private const int CapacityDefaultValue = 20;
+
+        public Satchel()
+            :base(CapacityDefaultValue)
+        {
+        }
+    }
+}
