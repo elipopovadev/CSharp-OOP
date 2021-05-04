3728685eb1bcddf1ee67f4f7910539d77f5fd9ac
Elena Popova <elipopovadev@gmail.com>
2021-05-04 14:22:50 +0200

    Bakery

---
+++ ExamPreparation/C# OOP Exam - 12 December 2020/Models/BakedFoods/BakeFood.cs
@@ -0,0 +1,62 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+
+namespace Bakery.Models.BakedFoods
+{
+    public abstract class BakeFood : IBakedFood
+    {
+        private string name;
+        private int portion;
+        private decimal price;
+
+        protected BakeFood(string name, int portion, decimal price)
+        {
+            this.Name = name;
+            this.Portion = portion;
+            this.Price = price;
+        }
+
+        public string Name
+        { 
+            get => name; 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName));
+                }
+
+                name = value;
+            }
+        }
+
+        public int Portion
+        {
+            get => portion;
+            set
+            {
+                if(portion <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPortion));
+                }
+
+                portion = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                if(price <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPrice));
+                }
+
+                price = value;
+            }
+        }
+    }
+}
