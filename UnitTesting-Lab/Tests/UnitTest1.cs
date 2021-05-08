3718420557647c46c6fda19a6b36daa3ff45ae3a
Elena Popova <elipopovadev@gmail.com>
2021-05-08 11:19:10 +0200

    NUnit testing

--- UnitTesting-Lab/Tests/UnitTest1.cs
+++ UnitTesting-Lab/Tests/UnitTest1.cs
@@ -6,19 +6,26 @@ namespace Tests
     [TestFixture]
     public class AxeTests
     {
+        private Dummy target;
+
+        [SetUp]
+       public void SetTarget()
+        {
+            target = new Dummy(20, 50); // replace with target
+        }
+
         [Test]
         public void AxeShouldLosesDurabilityAfterEachAttack()
         {
             //Arrange
             Axe axe = new Axe(30, 40);
-            Dummy target = new Dummy(20, 50);
 
             //Act
-            axe.Attack(target);
+            axe.Attack(this.target); // replace with target
             var result = axe.DurabilityPoints;
 
             //Assert
-            Assert.That(result, Is.EqualTo(39));
+            Assert.That(result, Is.EqualTo(39), "Axe loses durability after attack.");
         }
 
         [Test]
@@ -26,66 +33,68 @@ namespace Tests
         {
             //Arrage
             Axe axe = new Axe(10, 1);
-            Dummy target = new Dummy(20, 50);
 
             //Act
-            axe.Attack(target);
+            axe.Attack(this.target); // replace with target
 
             //Assert
             Assert.Throws<InvalidOperationException>(() => throw new InvalidOperationException("Axe is broken"));
         }
     }
 
-
+ 
     [TestFixture]
     public class DummyTest
     {
+        private Dummy aliveDummy;
+        private Dummy deadDummy;
+
+        [SetUp]
+        public void SetUpDummies ()
+        { 
+            deadDummy =  new Dummy(0, 10);
+            aliveDummy = new Dummy(40, 10);
+        }
+
         [Test]
         public void DummyShouldLosesHealthIfAttacked()
         {
             // Arrange
-            var dummy = new Dummy(40, 50);
-
             // Act
-            dummy.TakeAttack(30);
+            aliveDummy.TakeAttack(30);
 
             // Assert
-            Assert.That(dummy.Health, Is.EqualTo(10));
+            Assert.That(this.aliveDummy.Health, Is.EqualTo(10));
         }
 
         [Test]
         public void DeadDummyShouldThrowsExceptionIfAttacked()
         {
             // Arrange
-            var dummy = new Dummy(0, 10);
-
             // Act
             // Assert
-            Assert.That(() => dummy.TakeAttack(30), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+            Assert.That(() => this.deadDummy.TakeAttack(30), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."), "Dead Dummy can't be attacked");
         }
 
         [Test]
         public void DeadDummyCanGiveXP()
         {
             // Arrange
-            var dummy = new Dummy(0, 10);
 
             // Act
-            var experience = dummy.GiveExperience();
+            var experience = this.deadDummy.GiveExperience();
 
             // Assert
-            Assert.That(experience, !Is.EqualTo(null));
+            Assert.That(experience, Is.EqualTo(10), "Dead Dummy Can give experiance");
         }
 
         [Test]
         public void AliveDummyCantGiveXP()
         {
             // Arrange
-            var dummy = new Dummy(40, 10);
-
             // Act
             // Assert
-            Assert.That(() => dummy.GiveExperience(),Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+            Assert.That(() => this.aliveDummy.GiveExperience(),Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
         }
     }
 }
