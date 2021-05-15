5165e6d903b02a37eb03a77ab54ab7e888c971ac
Elena Popova <elipopovadev@gmail.com>
2021-05-15 21:08:06 +0200

    FightingArena

--- UnitTesting-Exercise/FightingArena.Tests/ArenaTests.cs
+++ UnitTesting-Exercise/FightingArena.Tests/ArenaTests.cs
@@ -19,7 +19,7 @@ namespace Tests
         }
 
         [Test]
-        public void ValidateIfArenaContainsCorrectWarriorsCountAfterEnrollCommand()
+        public void ValidateIfArenaIncreaseCountAfterEnrollCommand()
         {
             // Arrange
             // Act
