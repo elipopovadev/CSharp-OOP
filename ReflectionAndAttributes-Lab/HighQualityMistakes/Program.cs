10e1567630f62e5187dcc7f7b3afbff2b8b09b91
Elena Popova <elipopovadev@gmail.com>
2021-06-02 00:32:03 +0200

    HighQualityMistakes

---
+++ ReflectionAndAttributes-Lab/HighQualityMistakes/Program.cs
@@ -0,0 +1,15 @@
+﻿using Stealer;
+using System;
+
+namespace HighQualityMistakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.AnalyzeAcessModifiers("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
