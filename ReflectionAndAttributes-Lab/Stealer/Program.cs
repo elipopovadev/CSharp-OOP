b3e52ad86de0a1d1b2622b7aa3824c602286b9d0
Elena Popova <elipopovadev@gmail.com>
2021-06-01 23:47:06 +0200

    Stealer

--- ReflectionAndAttributes-Lab/Stealer/Program.cs
+++ ReflectionAndAttributes-Lab/Stealer/Program.cs
@@ -6,7 +6,9 @@ namespace Stealer
     {
         static void Main(string[] args)
         {
-           
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
         }
     }
 }
--- ReflectionAndAttributes-Lab/Stealer/Spy.cs
+++ ReflectionAndAttributes-Lab/Stealer/Spy.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Stealer
+﻿namespace Stealer
 {
-    class Spy
+    using System;
+    using System.Reflection;
+    using System.Text;
+   public class Spy
     {
+        public string StealFieldInfo(string classToInvestigate, params string[] fieldsToInvestigate)
+        {
+            Type type = typeof(Hacker);
+            var instance =  Activator.CreateInstance(type, new object[] { }) as Hacker;
+           
+            var sb = new StringBuilder();
+            sb.AppendLine($"Class under investigation: {type}");
+            foreach (var fieldAsString in fieldsToInvestigate)
+            {
+                var field = type.GetField($"{fieldAsString}", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+                var fieldValue = field.GetValue(instance);
+                sb.AppendLine($"{field.Name} = {fieldValue}");
+            }
+
+            string result = sb.ToString().TrimEnd();
+            return result;
+        }
     }
 }
