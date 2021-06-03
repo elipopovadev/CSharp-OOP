8b99990eabb88206d8a16b79bfc8c0cf0163d8c2
Elena Popova <elipopovadev@gmail.com>
2021-06-03 10:51:59 +0200

    Collector

---
+++ ReflectionAndAttributes-Lab/Collector/Program.cs
@@ -0,0 +1,15 @@
+﻿using Stealer;
+using System;
+
+namespace Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
